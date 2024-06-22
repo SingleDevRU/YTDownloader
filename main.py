70f64695974b14c812b3dc8c8730c45de83f8cba
SingleDevRu <boyarskydv@yandex.ru>
2024-06-22 15:22:12 +0500

    Добавлен input URL

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from pytube import YouTube
 
-yt = YouTube("https://www.youtube.com/watch?v=m2Ci5PT9rMk")
+path = input("Введите URL: ")
+yt = YouTube(path)
 
 stream = yt.streams.filter(only_audio=True).get_by_itag(251)
 stream.download("./Download")
